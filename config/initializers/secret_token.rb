6a755ea3aa3e5f7502c520f22e385602780238c0
Kyle Gonzales <kgonzal5@unm.edu>
2011-10-05 20:21:22 -0600

    ECE 595 Blog Project added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '66bae8e5f455907ff80861a5161e4e7977b91f3bd1548c07eb0f489e8659c5254ff61184a587e6da001909f93d84df5fea2a401c85bc127cf3d2a52382f95fd9'
